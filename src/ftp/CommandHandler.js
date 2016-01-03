48a86f496e01647c8618b3d3519bdf910ea5e18e
Rich McNeary <rlm2public@gmail.com>
2016-01-03 15:03:46 -0500

    USER error handling. Commands with no arguments. Implement PASS.

--- src/ftp/CommandHandler.js
+++ src/ftp/CommandHandler.js
@@ -29,25 +29,24 @@ class CommandHandler {
 
                 var handlerName = command.command.toLowerCase();
                 if (!_supportedCommands.hasOwnProperty(handlerName)) {
-                    console.log(`CommandHandler.js handleRequest() - "${command.command}" is not supported.`);
+                    console.log(`CommandHandler.js handleRequest() - "${command.command}" is not implemented.`);
 
-                    // TODO send error response to client.
-
-                    return {
-                        command,
-                        error: `"${command.command}" is not supported.`
-                    };
+                    // Send not implemented error response to client.
+                    return Promise.resolve(sendHandler("502 Command not implemented.\r\n"))
+                        .then(() => { return `"${command.command}" is not implemented.`; });
                 }
 
-                return Promise.resolve(_supportedCommands[handlerName](server, state, command, sendHandler))
+                return Promise.resolve(_supportedCommands[handlerName](server, state, command, sendHandler));
             })
             .then(error => {
                 var result = {
                     command
                 };
 
-                if (typeof error === "string") {
+                if (typeof error !== "undefined" && error !== null) {
                     result.error = error;
+                } else {
+                    state.lastCommand = command.command;
                 }
 
                 return result;
@@ -61,27 +60,60 @@ var _supportedCommands = {
 
     user(server, state, command, sendHandler) {
         console.log(`CommandHandler.js user() - "${command.command}".`);
-        if (typeof state.lastCommand !== "undefined" || state.lastCommand !== null) {
-            // TODO: It's an error if USER is not the first command received.
+
+        var response = null;
+        if (typeof state.lastCommand !== "undefined" && state.lastCommand !== null) {
+            // It's an error if USER is not the first command received.
+            response = "503 USER required first.\r\n";
         }
 
         if (typeof state.user !== "undefined") {
-            // TODO: It's an error if USER has already been sent.
+            // It's an error if USER has already been sent.
+            response = "503 already set.\r\n";
+        }
+
+        // Always allow login to proceed from user. If the username and / or password are incorrect an error is returned in pass().
+        if (response === null) {
+            var allowAnonymous = server.getAllowAnonymousLogin();
+            response = `331 Anonymous login ${allowAnonymous ? "is" : "is not"} allowed.\r\n`;
+            state.user = allowAnonymous ? server.getUsername() : command.argument;
         }
         
-        state.user = command.argument;
-        return Promise.resolve(sendHandler(`331 anonymous login ${server.getAllowAnonymousLogin() ? "is" : "is not"} allowed.\r\n`))
+        return Promise.resolve(sendHandler(response))
             .then(() => { return; });
     },
 
     pass(server, state, command, sendHandler) {
         console.log(`CommandHandler.js pass() - "${command.command}".`);
+
+        var response = "230 User logged in, proceed.\r\n";
+        if (state.lastCommand !== "USER") {
+            // It's an error if USER is not the previous command received.
+            response = "503 USER required first.\r\n";
+        }
+        else {
+            if (!server.getAllowAnonymousLogin()) {
+                if (state.user !== server.getUsername() || command.argument !== server.getPassword()) {
+                    delete state.user;
+                    response = "530 Username and/or password is incorrect.\r\n";
+                } else {
+                    state.loggedIn = true;
+                }
+            } else {
+                state.anonymous = command.argument;
+                state.loggedIn = true;
+            }
+        }
+
+        return Promise.resolve(sendHandler(response))
+            .then(() => { return; });
     }
 }
 
 
 function createCommandRequest(request) {
     var commandSeparatorIndex = request.indexOf(" ");
+    commandSeparatorIndex = 0 <= commandSeparatorIndex ? commandSeparatorIndex : request.indexOf("\r\n"); // Check for command with no arguments.
     var valid = 0 < commandSeparatorIndex;
     var result = {
         request,
@@ -90,7 +122,7 @@ function createCommandRequest(request) {
 
     if (valid) {
         result.argument = request.substring(commandSeparatorIndex + 1, request.length - 2); // Don't include the \r\n
-        result.command = request.substring(0, commandSeparatorIndex);
+        result.command = request.substring(0, commandSeparatorIndex).toUpperCase();
     }
 
     return result;
