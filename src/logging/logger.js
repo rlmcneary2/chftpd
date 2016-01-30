da52780cc765277f2f034d673ed8db747e8735ee
Rich McNeary <rlm2public@gmail.com>
2016-01-29 22:22:55 -0500

    Add verbose and warning. Log with styles.

--- src/logging/logger.js
+++ src/logging/logger.js
@@ -3,16 +3,49 @@
 
 module.exports = {
 
+    level: 0,
+    
+    levels: Object.freeze({
+        error: 0,
+        warning: 1,
+        info: 2,
+        debug: 3,
+        verbose: 4
+    }),
+
     /**
-     * Pass a template string containing an informational message.
+     * Pass a string containing an informational message.
      */
     info() {
-        var parts = arguments[0];
-        var message = parts[0];
-        for (var i = 1; i < arguments.length; ++i) {
-            message += arguments[i] + parts[i - 1];
+        if (this.levels.info < this.level){
+            return;
         }
 
+        var message = arguments[0];
         console.log(message);
+    },
+    
+    /**
+     * Pass a string containing a message.
+     */
+    verbose() {
+        if (this.levels.verbose < this.level){
+            return;
+        }
+
+        var message = arguments[0];
+        console.log("%c" + message, "color:#BEBEBE; font-style:italic");
+    },
+    
+    /**
+     * Pass a string containing a warning message.
+     */
+    warning(){
+        if (this.levels.warning < this.level){
+            return;
+        }
+
+        var message = arguments[0];
+        console.log("%c" + message, "color:#998C2C; font-style:bold");
     }
 };
